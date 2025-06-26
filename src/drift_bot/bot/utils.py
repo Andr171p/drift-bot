898a518adf758f611afa4794c99f38373967bba8
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-06-26 19:53:58 +0500

    26.06.2025 Fix file reading

--- src/drift_bot/bot/utils.py
+++ src/drift_bot/bot/utils.py
@@ -10,7 +10,7 @@ async def get_file(file_id: str, call: CallbackQuery) -> File:
     file = await call.bot.get_file(file_id=file_id)
     data = await call.bot.download(file)
     file_name = file.file_path
-    return File(data=data, file_name=file_name)
+    return File(data=data.read(), file_name=file_name)
 
 
 def draw_progress_bar(filled: int, total: int, width: int = WIDTH) -> str:
